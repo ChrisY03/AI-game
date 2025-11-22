0a2b202b8c640dd21b9c1a4355e5607355130e80
Pierce74 <pierceriordan12@gmail.com>
2025-11-22 18:14:41 +0000

    Update BirdFlock.gd

--- ai_demo/scripts/BirdFlock.gd
+++ ai_demo/scripts/BirdFlock.gd
@@ -8,19 +8,26 @@ var center: Vector3
 var angle: float = 0.0
 var bird: Node3D
 
+@onready var birds_node: Node = $Birds
+
+
 func _ready() -> void:
-	var birds_node: Node = get_node("Birds")
 	if birds_node == null:
-		push_error("BirdFlock ERROR: Could not find 'Birds' node!")
+		push_error("BirdFlock ERROR: Missing 'Birds' node!")
 		return
 
+	# get the first child (your model)
 	bird = birds_node.get_child(0)
 	if bird == null:
-		push_error("BirdFlock ERROR: Birds has no children!")
+		push_error("BirdFlock ERROR: 'Birds' has no children!")
 		return
 
+	# set the orbit center
 	center = global_position
+
+	# place the bird initially
 	bird.global_position = center + Vector3(0, height, orbit_radius)
+
 
 func _physics_process(delta: float) -> void:
 	if bird == null:
@@ -28,13 +35,14 @@ func _physics_process(delta: float) -> void:
 
 	angle += orbit_speed * delta
 
+	# orbit position
 	var x := center.x + orbit_radius * cos(angle)
 	var z := center.z + orbit_radius * sin(angle)
 	var y := center.y + height
 
 	bird.global_position = Vector3(x, y, z)
 
-	# Face forward along orbit path
+	# forward-facing direction
 	var forward_offset := angle + 0.1
 	var fx := center.x + orbit_radius * cos(forward_offset)
 	var fz := center.z + orbit_radius * sin(forward_offset)
